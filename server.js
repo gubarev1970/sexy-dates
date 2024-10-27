1e81edc4036fdee80b601b256a48546dfa04e01d
gubarev1970 <gubarev1970@seznam.cz>
2024-10-27 16:47:05 +0100

    sexy-dates

--- server.js
+++ server.js
@@ -1,38 +1,19 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // URL z Render.com
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
-const express = require('express');
-const { addUser, getUsers } = require('./userModel'); // Import funkcí z userModel.js
-const app = express();
-app.use(express.json());
+// Funkce pro přidání uživatele
+const addUser = async (username, email, password) => {
+  const result = await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *', [username, email, password]);
+  return result.rows[0];
+};
 
-// Endpoint pro základní URL
-app.get('/', (req, res) => {
-  res.send('Aplikace běží!'); // Odpověď na požadavek na /
-});
+// Funkce pro získání všech uživatelů
+const getUsers = async () => {
+  const result = await pool.query('SELECT * FROM users');
+  return result.rows;
+};
 
-// Endpoint pro registraci uživatele
-app.post('/register', async (req, res) => {
-  const { username, email, password } = req.body;
-  const newUser = await addUser(username, email, password);
-  if (newUser) {
-    res.status(201).json(newUser);
-  } else {
-    res.status(500).send('Chyba při registraci');
-  }
-});
-
-// Endpoint pro získání všech uživatelů
-app.get('/users', async (req, res) => {
-  const users = await getUsers();
-  res.json(users);
-});
-
-// Port pro naslouchání
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Server běží na portu ${PORT}`);
-});
+module.exports = { addUser, getUsers };
