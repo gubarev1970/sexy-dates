6876c0775b2d525507816ce3afeb6b5123b14154
gubarev1970 <gubarev1970@seznam.cz>
2024-10-29 09:51:59 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 const pool = new Pool({
   user: 'postgres',
